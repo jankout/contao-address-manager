5bf90e4e190d445cf4b8d942b5aacf43fb1d96f1
Jan Kout <unocure@gmail.com>
2025-07-05 23:19:13 +0200

    use Contao

--- contao/config/config.php
+++ contao/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 // Register the model (optional)
-$GLOBALS['TL_MODELS']['tl_addresses'] = \JanKout\ContaoAddressManager\Model\AddressModel::class;
+use JanKout\ContaoAddressManager\Model\AddressModel;
 
 $GLOBALS['BE_MOD']['content']['addresses'] = [
     'tables' => ['tl_addresses'],
--- src/Model/AddressModel.php
+++ src/Model/AddressModel.php
@@ -1,10 +1,20 @@
 <?php
-
 namespace JanKout\ContaoAddressManager\Model;
 
+use Contao\Model\Collection;
+use Contao\Database;
+use Contao\Date;
+use Contao\FilesModel;
 use Contao\Model;
+use Contao\NewsModel;
+use Contao\System;
 
 class AddressModel extends Model
 {
+    /**
+     * Table name.
+     *
+     * @var string
+     */
     protected static $strTable = 'tl_addresses';
 }
