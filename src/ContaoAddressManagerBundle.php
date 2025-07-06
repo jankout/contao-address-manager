7656488f5aff6ced9ba0df509e97041cb9aafefb
Jan Kout <unocure@gmail.com>
2025-07-06 12:26:28 +0200

    korrektur der Class

--- src/ContaoAddressManagerBundle.php
+++ src/ContaoAddressManagerBundle.php
@@ -1,6 +1,7 @@
-<?php
+<?php declare(strict_types=1);
+
 namespace JanKout\ContaoAddressManager;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class JanKoutContaoAddressManagerBundle extends Bundle {}
+class ContaoAddressManagerBundle extends Bundle {}
