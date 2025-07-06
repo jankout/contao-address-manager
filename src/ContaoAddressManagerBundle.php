d7059dd3667ba1274c1b0d67a2c00c463f015184
Jan Kout <unocure@gmail.com>
2025-07-06 12:45:13 +0200

    class

--- src/ContaoAddressManagerBundle.php
+++ src/ContaoAddressManagerBundle.php
@@ -4,4 +4,4 @@ namespace JanKout\ContaoAddressManager;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class JanKoutContaoAddressManagerBundle extends Bundle {}
+class ContaoAddressManagerBundle extends Bundle {}
