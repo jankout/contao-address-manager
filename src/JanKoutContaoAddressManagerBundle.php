b75361b1e69181d58da4db58ff9ca0e8430eeb3d
Jan Kout <unocure@gmail.com>
2025-07-06 14:03:36 +0200

    löschung

--- src/JanKoutContaoAddressManagerBundle.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace JanKout\ContaoAddressManager;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class JanKoutContaoAddressManagerBundle extends Bundle {}
